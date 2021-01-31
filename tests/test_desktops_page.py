05d70becb1befff26134b2b7c3ac2f0477f595c5
Anton Zhulanov <zhulanoff2007@gmail.com>
2021-02-01 01:42:03 +0300

    переопределение фикстуры browser для уменьшения количества передаваемых параметров и вызовов метода для перехода на необходимую страницу

--- tests/test_desktops_page.py
+++ tests/test_desktops_page.py
@@ -5,46 +5,48 @@ import pytest
 
 
 @pytest.fixture
-def base_url(base_url):
+def category_url(base_url):
     return f"{base_url}/index.php?route=product/category&path=20"
 
 
-def test_browser_title_is_desktops(base_url, browser):
+@pytest.fixture
+def browser(browser, category_url):
+    browser.get(category_url)
+    return browser
+
+
+def test_browser_title_is_desktops(category_url, browser):
     """
      проверка заголовка страницы Desktops
     """
-    browser.get(f"{base_url}")
-    assert base_url == browser.current_url
+    assert category_url == browser.current_url
     assert "Desktops" in browser.title
 
 
-def test_text_of_header_is_desktops(base_url, browser):
+def test_text_of_header_is_desktops(browser):
     """
     проверка наличия заголовка Desktops в странице
     """
-    browser.get(f"{base_url}")
     assert WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.TAG_NAME, "h2"))
     )
     assert browser.find_element_by_xpath("//*[@id='content']/h2").text == "Desktops"
 
 
-def test_left_menu_present(base_url, browser):
+def test_left_menu_present(browser):
     """
     проверка отображения меню слева
     """
-    browser.get(f"{base_url}")
     assert WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.ID, "column-left"))
     )
 
 
-def test_text_description_is_present(base_url, browser):
+def test_text_description_is_present(browser):
     """
     проверка отображения описания блока
     """
     description_xpath = "//*[@id='content']/div[1]/div[2]/p"
-    browser.get(f"{base_url}")
     assert WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.XPATH, description_xpath))
     )
@@ -52,11 +54,10 @@ def test_text_description_is_present(base_url, browser):
     assert description.text == "Example of category description text"
 
 
-def test_count_elements_in_main_block_equals_twelve(base_url, browser):
+def test_count_elements_in_main_block_equals_twelve(browser):
     """
     проверка количества блоков в разделе Desktops
     """
-    browser.get(f"{base_url}")
     items = browser.find_elements_by_xpath("//*[@id='content']/div[4]/div")
     assert len(items) == 12
 
--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -5,46 +5,48 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture
-def base_url(base_url):
+def product_url(base_url):
     return f"{base_url}/index.php?route=product/product&path=57&product_id=49"
 
 
-def test_browser_title_of_product(base_url, browser):
+@pytest.fixture
+def browser(product_url, browser):
+    browser.get(product_url)
+    return browser
+
+
+def test_browser_title_of_product(product_url, browser):
     """
      проверка заголовка браузера на странице продукта
     """
-    browser.get(f"{base_url}")
-    assert base_url == browser.current_url
+    assert product_url == browser.current_url
     assert "Samsung Galaxy Tab 10.1" in browser.title
 
 
-def test_header_of_page(base_url, browser):
+def test_header_of_page(browser):
     """
     проверка отображения заголовка товара в странице продукта
     """
-    browser.get(f"{base_url}")
     header_of_product_xpath = "//*[@id='content']/div/div[2]/h1"
     header_of_product = browser.find_element_by_xpath(header_of_product_xpath)
     assert header_of_product.text == "Samsung Galaxy Tab 10.1"
 
 
 @pytest.mark.parametrize("name, idx", [("Product Code:", 0), ("Reward Points:", 1), ("Availability:", 2)])
-def test_features_of_product(base_url, browser, name, idx):
+def test_features_of_product(browser, name, idx):
     """
     проверка кода, премии, доступного количества продукта
     """
-    browser.get(f"{base_url}")
     features_of_product_xpath = "//*[@id='content']/div/div[2]/ul[1]/li"
     features_of_product = browser.find_elements_by_xpath(features_of_product_xpath)
     assert len(features_of_product) == 3
     assert name in features_of_product[idx].text
 
 
-def test_main_image_of_product_is_present(base_url, browser):
+def test_main_image_of_product_is_present(browser):
     """
     проверка отображения изображения продукта
     """
-    browser.get(f"{base_url}")
     main_img_xpath = "//*[@id='content']/div/div[1]/ul[1]/li[1]/a/img"
     assert WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.XPATH, main_img_xpath))
@@ -53,11 +55,10 @@ def test_main_image_of_product_is_present(base_url, browser):
     assert main_img.get_attribute("src").endswith(".jpg")
 
 
-def test_price_of_product(base_url, browser):
+def test_price_of_product(browser):
     """
     проверка цены продукта на валюту и минус
     """
-    browser.get(f"{base_url}")
     price_xpath = "//*[@id='content']/div/div[2]/ul[2]/li[1]/h2"
     currency_xpath = "//*[@id='form-currency']/div/button/strong"
 
