1a566c1501ca6517ff667542dd0fc563e221b456
Anton Zhulanov <zhulanoff2007@gmail.com>
2021-01-31 23:24:46 +0300

    обновил тест проверки на главной страницы: 1. Проверка заголовка 2. создание скриншота

--- tests/test_open_cart.py
+++ tests/test_open_cart.py
@@ -1,12 +1,4 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
 def test_main_page(browser, base_url):
     browser.get(base_url)
     assert "Your Store" in browser.title
-    assert WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.ID, "cart"))
-    )
     browser.save_screenshot(f"{browser.name}.png")
