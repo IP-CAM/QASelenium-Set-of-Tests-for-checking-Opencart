2441edc7fb734819273201bba67f51e6330c1016
Anton Zhulanov <zhulanoff2007@gmail.com>
2021-02-03 01:28:07 +0300

    доработка ожидания в хэлпере

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,5 +1,11 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 def assert_displayed_unique_element(browser, xpath):
+    assert WebDriverWait(browser, 10).until(
+        EC.visibility_of_any_elements_located((By.XPATH, xpath))
+    )
     elements = browser.find_elements_by_xpath(xpath)
     assert len(elements) == 1
-    element = elements[0]
-    assert element.is_displayed()
