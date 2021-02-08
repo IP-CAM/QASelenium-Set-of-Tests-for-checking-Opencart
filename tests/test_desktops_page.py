61144a72afa93d0f1643eaf82a79e447442d9c29
Anton Zhulanov <zhulanoff2007@gmail.com>
2021-02-09 01:19:25 +0300

    перевел локаторы на CSS

--- tests/test_desktops_page.py
+++ tests/test_desktops_page.py
@@ -25,10 +25,8 @@ def test_text_of_header_is_desktops(browser):
     """
     проверка наличия заголовка Desktops в странице
     """
-    h2_xpath = "//*[@id='content']/h2"
-    get_displayed_unique_element(browser, h2_xpath)
-
-    h2 = browser.find_element_by_xpath(h2_xpath)
+    h2_css = "#content > h2"
+    h2 = get_displayed_unique_element(browser, h2_css)
     assert h2.text == "Desktops"
 
 
@@ -36,26 +34,29 @@ def test_left_menu_present(browser):
     """
     проверка отображения меню слева
     """
-    column_left_xpath = "//*[@id='column-left']"
-    get_displayed_unique_element(browser, column_left_xpath)
+    column_left_css = "#column-left"
+    menu_items_css = "a"
+    menu = get_displayed_unique_element(browser, column_left_css)
+    menu_items = menu.find_elements_by_css_selector(menu_items_css)
+    assert len(menu_items) == 11
 
 
 def test_text_description_is_present(browser):
     """
     проверка отображения описания блока
     """
-    description_xpath = "//*[@id='content']/div[1]/div[2]/p"
-    get_displayed_unique_element(browser, description_xpath)
-    description = browser.find_element_by_xpath(description_xpath)
+    description_css = "#content > div:nth-child(2) > div.col-sm-10 > p"
+    description = get_displayed_unique_element(browser, description_css)
     assert description.text == "Example of category description text"
 
 
-def test_count_elements_in_main_block_equals_twelve(browser):
+def test_count_elements_in_main_block(browser):
     """
     проверка количества блоков в разделе Desktops
     """
-    items = browser.find_elements_by_xpath("//*[@id='content']/div[4]/div")
+    items = browser.find_elements_by_css_selector("#content > div:nth-child(7) > div")
     assert len(items) == 12
 
-    count_of_showing_elements = browser.find_element_by_xpath("//*[@id='content']/div[5]/div[2]")
+    count_of_showing_elements = browser.find_element_by_css_selector("#content > div:nth-child(8) > "
+                                                                     "div.col-sm-6.text-right")
     assert count_of_showing_elements.text == "Showing 1 to 12 of 12 (1 Pages)"
