3e80b81040b29bb6a88ed50548312bb640a4a34b
Anton Zhulanov <zhulanoff2007@gmail.com>
2021-02-08 23:49:14 +0300

    оптимизация assert_displayed_unique_element

--- tests/helpers.py
+++ tests/helpers.py
@@ -4,8 +4,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 def assert_displayed_unique_element(browser, xpath):
-    assert WebDriverWait(browser, 10).until(
+    elements = WebDriverWait(browser, 10).until(
         EC.visibility_of_any_elements_located((By.XPATH, xpath))
     )
-    elements = browser.find_elements_by_xpath(xpath)
     assert len(elements) == 1
