7d006fead50de27ae5af6216d005df54a19b2720
Anton Zhulanov <zhulanoff2007@gmail.com>
2021-02-09 01:48:32 +0300

    перевел локаторы на CSS

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -19,31 +19,32 @@ def test_is_main_carousel_present(browser):
     """
     проверка отображения главного баннера
     """
-    main_banner_xpath = "//*[@id='common-home']"
-    get_displayed_unique_element(browser, main_banner_xpath)
+    main_banner_css = "#slideshow0 > div > div.swiper-slide.text-center.swiper-slide-active > img"
+    banner_image = get_displayed_unique_element(browser, main_banner_css)
+    banner_image.get_attribute('src').endswith('.jpg')
 
 
 def test_count_elements_in_featured_row_equals_four(browser):
     """
     проверка количества блоков в главном баннере
     """
-    items = browser.find_elements_by_xpath("//*[@id='content']/div[2]/div")
+    items = browser.find_elements_by_css_selector("#slideshow0 > div > div")
     assert len(items) == 4
 
 
-def test_is_second_carousel_present(browser):
+def test_is_second_carousel_items(browser):
     """
     проверка отображения баннера с логотипами
     """
-    second_banner_xpath = "//*[@id='carousel0']"
-    get_displayed_unique_element(browser, second_banner_xpath)
+    second_banner_css = "#carousel0"
+    second_banner = browser.find_element_by_css_selector(second_banner_css)
+    second_banner.is_displayed()
 
 
 def test_text_of_header_is_featured(browser):
     """
     проверка наличия заголовка Featured
     """
-    h3_xpath = "//*[@id='content']/h3"
-    h3 = browser.find_element_by_xpath(h3_xpath)
-    get_displayed_unique_element(browser, h3_xpath)
+    h3_css = "#content > h3"
+    h3 = get_displayed_unique_element(browser, h3_css)
     assert h3.text == "Featured"
