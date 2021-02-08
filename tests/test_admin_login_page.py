b41efa6b05a7d5cfe1edee45c512ddfe8e89b44f
Anton Zhulanov <zhulanoff2007@gmail.com>
2021-02-09 01:26:29 +0300

    перевел локаторы на CSS

--- tests/test_admin_login_page.py
+++ tests/test_admin_login_page.py
@@ -25,40 +25,27 @@ def test_header_of_block_login(browser):
     """
     проверка отображения заголовка с текстом о вводе логина и пароля
     """
-    header_admin_login_xpath = "//*[@id='content']/div/div/div/div/div[1]/h1"
-    get_displayed_unique_element(browser, header_admin_login_xpath)
-
-    header_admin_login = browser.find_element_by_xpath(header_admin_login_xpath)
-
-    assert header_admin_login.text == "Please enter your login details."
+    header_admin_login_css = "#content > div > div > div > div > div.panel-heading > h1"
+    header_login = get_displayed_unique_element(browser, header_admin_login_css)
+    assert header_login.text == "Please enter your login details."
 
 
 def test_field_username_is_visible(browser):
     """проверка отображения поля ввода имени пользователя"""
-    field_username_xpath = "//*[@id='input-username']"
-
-    get_displayed_unique_element(browser, field_username_xpath)
-
-    field_username = browser.find_element_by_xpath(field_username_xpath)
-
+    field_username_css = "#input-username"
+    field_username = get_displayed_unique_element(browser, field_username_css)
     assert field_username.get_attribute("placeholder") == "Username"
 
 
 def test_field_password_is_visible(browser):
     """проверка отображения поля ввода пароля пользователя"""
-    field_password_xpath = "//*[@id='input-password']"
-    get_displayed_unique_element(browser, field_password_xpath)
-
-    field_password = browser.find_element_by_xpath(field_password_xpath)
-
+    field_password_css = "#input-password"
+    field_password = get_displayed_unique_element(browser, field_password_css)
     assert field_password.get_attribute("placeholder") == "Password"
 
 
 def test_button_login_is_visible(browser):
     """проверка отображения кнопки авторизации пользователя"""
-    button_login_xpath = "//*[@id='content']/div/div/div/div/div[2]/form/div[3]/button"
-    get_displayed_unique_element(browser, button_login_xpath)
-
-    button_login = browser.find_element_by_xpath(button_login_xpath)
-
+    button_login_css = "#content > div > div > div > div > div.panel-body > form > div.text-right > button"
+    button_login = get_displayed_unique_element(browser, button_login_css)
     assert button_login.text == "Login"
