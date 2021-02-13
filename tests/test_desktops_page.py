8b1935978e1c593bdae3f100e1901a5ec0613756
Anton Zhulanov <zhulanoff2007@gmail.com>
2021-02-14 00:50:53 +0300

    адапатция тестов страницы с категорией desktops на структуру PageObject

--- tests/test_desktops_page.py
+++ tests/test_desktops_page.py
@@ -1,62 +1,36 @@
 import pytest
-from helpers import get_displayed_unique_element
+from pages.CategoryPage import CategoryPage
 
 
 @pytest.fixture
-def category_url(base_url):
-    return f"{base_url}/index.php?route=product/category&path=20"
+def desktops_url(base_url):
+    return f"{base_url}/desktops"
 
 
 @pytest.fixture
-def browser(browser, category_url):
-    browser.get(category_url)
-    return browser
+def desktops_page(browser, desktops_url):
+    page = CategoryPage(browser, desktops_url)
+    page.open()
+    return page
 
 
-def test_browser_title_is_desktops(category_url, browser):
-    """
-     проверка заголовка страницы Desktops
-    """
-    assert category_url == browser.current_url
-    assert "Desktops" in browser.title
+def test_browser_title_is_desktops(desktops_page):
+    """проверка заголовка страницы Desktops"""
+    assert desktops_page.get_title() == "Desktops"
 
 
-def test_text_of_header_is_desktops(browser):
-    """
-    проверка наличия заголовка Desktops в странице
-    """
-    h2_css = "#content > h2"
-    h2 = get_displayed_unique_element(browser, h2_css)
-    assert h2.text == "Desktops"
+def test_text_of_header_of_category_page(desktops_page):
+    """проверка наличия заголовка Desktops в странице"""
+    assert desktops_page.get_header_category().text == "Desktops"
 
 
-def test_left_menu_present(browser):
-    """
-    проверка отображения меню слева
-    """
-    column_left_css = "#column-left"
-    menu_items_css = "a"
-    menu = get_displayed_unique_element(browser, column_left_css)
-    menu_items = menu.find_elements_by_css_selector(menu_items_css)
-    assert len(menu_items) == 11
+def test_text_description(desktops_page):
+    """проверка отображения описания блока"""
+    assert desktops_page.get_description_category().text == "Example of category description text"
 
 
-def test_text_description_is_present(browser):
-    """
-    проверка отображения описания блока
-    """
-    description_css = "#content > div:nth-child(2) > div.col-sm-10 > p"
-    description = get_displayed_unique_element(browser, description_css)
-    assert description.text == "Example of category description text"
-
-
-def test_count_elements_in_main_block(browser):
-    """
-    проверка количества блоков в разделе Desktops
-    """
-    items = browser.find_elements_by_css_selector("#content > div:nth-child(7) > div")
+def test_count_elements_in_category(desktops_page):
+    """проверка количества блоков в разделе Desktops"""
+    items = desktops_page.get_items_in_category()
     assert len(items) == 12
-
-    count_of_showing_elements = browser.find_element_by_css_selector("#content > div:nth-child(8) > "
-                                                                     "div.col-sm-6.text-right")
-    assert count_of_showing_elements.text == "Showing 1 to 12 of 12 (1 Pages)"
+    assert desktops_page.get_text_of_showing_block() == "Showing 1 to 12 of 12 (1 Pages)"
